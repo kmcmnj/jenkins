cd2b88aad2b16ef48f7d674f43f0585502750a28
dell <dell@DESKTOP-Q1RIKB8>
2022-11-06 10:38:03 +0530

    Jenkins

---
+++ src/test/java/Scripts/Test3.java
@@ -0,0 +1,12 @@
+package Scripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test3 {
+	@Test
+	public void tc3() {
+		Reporter.log("Hello Java",true);
+	}
+
+}
